df00e160abf29eca87e7d2933055fc71a2aa9c9a
maiime <991661026@qq.com>
2018-05-12 16:06:27 +0800

    add rating & my invite

---
+++ js/setRem.js
@@ -0,0 +1,22 @@
+/**
+ * author: mawenjie
+ * last modify: 2018-05-12 14:48:25
+ */
+// set rem
+(function() {
+    function resetRem() {
+        var _width = window.innerWidth;
+        var html = document.getElementsByTagName('html')[0];
+        if (_width > 750) {
+            html.style.width = '750px';
+            html.style.margin = '0 auto';
+            html.style.fontSize = '100px';
+        } else {
+            html.style.fontSize = _width / 7.5 + 'px';
+        }
+    }
+    resetRem();
+    window.onresize = function () {
+        resetRem();
+    };
+})();
