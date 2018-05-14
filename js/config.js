6179b042e99f1ca9a824c1465c2add20c2458cf1
maiime <991661026@qq.com>
2018-05-14 18:21:10 +0800

    change invite template image

--- js/config.js
+++ js/config.js
@@ -1,20 +1,24 @@
 ;(function () {
     const config = {
         downloadAppLink: 'http://www.baidu.com',
-        host: 'http://api-dev.xiaoyunquegroup.com/articles',
+        host: 'https://api-dev.xiaoyunquegroup.com/articles',
         jsSkd: {
             onMenuShareAppMessage: {
-                title: '我是title',
+                title: '分享给朋友',
                 desc: '我是描述',
                 link: 'http://www.baidu.com',
                 imgUrl: 'http://mat1.gtimg.com/www/qq2018/imgs/qq_logo_2018.png',
-                success: function () {}
+                success: function () {
+                    console.log('success');
+                }
             },
             onMenuShareTimeline: {
-                title: '',
-                link: '',
-                imgUrl: '',
-                success: function () {}
+                title: '分享朋友圈',
+                link: 'http://www.baidu.com',
+                imgUrl: 'http://mat1.gtimg.com/www/qq2018/imgs/qq_logo_2018.png',
+                success: function () {
+                    console.log('success');
+                }
             }
         }
     };
